af0956e1b08c17f5518b340517306bbdc9ebb7a3
MaazMohammedM <maazmohammed742@gmail.com>
2025-11-10 15:12:09 +0530

    implement eventlisterner on numbers and operators and add display function for displaying calculation and add result display function

--- index.js
+++ index.js
@@ -26,7 +26,67 @@ function operate(operater,a,b){
     }
 }
 
-console.log(operate("+",2,3));
-console.log(operate("-",2,3));
-console.log(operate("*",2,3));
-console.log(operate("/",2,3));
+let numBtn = document.querySelectorAll('.num');
+let operaterBtns = document.querySelectorAll('.operator');
+let equalBtn = document.querySelector('.equal');
+let clearBtn = document.querySelector('.clear');
+let resultDisplay = document.querySelector('.result');
+let allClearBtn = document.querySelector('.allClear');
+
+let computableStr = '';
+let calculationDisplay = document.querySelector('.calculation');
+
+
+numBtn.forEach((btn)=>{
+    btn.addEventListener('click',function(){
+        displayCalc(btn)
+    })
+})
+operaterBtns.forEach((btn)=>{
+    btn.addEventListener('click',function(){
+        displayCalc(btn)
+    })
+})
+
+function displayCalc(btn){
+    computableStr+=btn.innerText;
+        calculationDisplay.innerText = computableStr;
+}
+
+equalBtn.addEventListener('click',function(){
+    let operater = '';
+    if(computableStr.includes('+')){
+        operater = "+"
+    } else if(computableStr.includes("-")){
+        operater = '-'
+    } else if(computableStr.includes("*")){
+        operater = "*"
+    } else if(computableStr.includes('/')){
+        operater = "/"
+    }
+
+    let splitedArr = computableStr.split(operater);
+    let numOne = +splitedArr[0];
+    let numTwo = +splitedArr[1];
+
+    console.log(numOne, numTwo, operater)
+
+    let result = operate(operater,numOne,numTwo);
+    resultDisplay.innerText = result;
+
+
+})
+clearBtn.addEventListener('click',function(){
+    computableStr = computableStr.slice(0,-1);
+    calculationDisplay.innerText = computableStr;
+    console.log(computableStr)
+})
+
+allClearBtn.addEventListener('click',function(){
+    computableStr = '';
+    calculationDisplay.innerText = '';
+    resultDisplay.innerText = '';
+})
+
+
+
